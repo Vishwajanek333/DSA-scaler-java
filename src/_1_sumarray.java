83938ed5b8951030e931e2e8c962cc41160ffa5f
Vishwajanek <vishwajrevacollegeaiandds@gmail.com>
2025-06-16 19:25:01 +0530

    file

---
+++ src/_1_sumarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class _1_sumarray {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+
+        int sum=0;
+
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] =sc.nextInt();
+        }
+
+        // main logic
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.print(sum + " ");
+    }
+}
