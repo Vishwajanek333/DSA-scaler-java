6493b453a2f6f5aa3e00b0f9591b543ae2e5fac8
Vishwajanek <vishwajrevacollegeaiandds@gmail.com>
2025-06-20 12:28:17 +0530

    Initial commit

--- src/_10_strings.java
+++ src/_10_strings.java
@@ -1,2 +1,35 @@
+import java .util.*;
 public class _10_strings {
+    public static void main(String [] args) {
+        String s = "vishwa";
+
+        //1.print
+        System.out.println(s);
+
+        // 2.to get the length
+        System.out.println(s.length());
+
+        //3.to get
+        System.out.println(s.charAt(2));
+
+        // 4.set: directly is not possible but  u can use char
+        // Convert to char array
+        char[] chars = s.toCharArray();
+        chars[2] = 'a';
+        // Convert back to String
+        s = new String(chars);
+        System.out.println(s);
+
+        // 5.search char
+        char ch = 's';
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ch) {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+
+        //6.
+
+    }
 }
---
+++ src/_11_reverse_string.java
@@ -0,0 +1,18 @@
+public class _11_reverse_string {
+    public static void main(String[] args){
+
+        removeVowels();
+    }
+    public static void removeVowels(){
+        String st ="welcome";
+        for(int i =0;i<st.length();i++){
+            char ch =st.charAt(i);
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    continue;
+
+                }
+            System.out.print(ch);
+        }
+
+    }
+}
